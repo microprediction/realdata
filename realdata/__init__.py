a309a1372daaa9c799bf4bf0be1a3369941c120f
Peter Cotton <peter.cotton@microprediction.com>
2020-12-31 16:55:20 -0500

    blah

--- realdata/__init__.py
+++ realdata/__init__.py
@@ -4,21 +4,21 @@ from microprediction import MicroReader
 import random
 
 
-DEFAULT_STREAMS = ['electricity-lbmp-nyiso-west.json','electricity-load-nyiso-nyc.json']
+EXAMPLE_STREAMS = ['electricity-lbmp-nyiso-west.json','electricity-load-nyiso-nyc.json']
 
 
 def random_name():
     """ Choose a random name of a stream """
     mr = MicroReader()
-    NAMES = mr.get_stream_names()
-    return random.choice(NAMES)
+    names = [n for n in mr.get_stream_names() if not '~' in n ]
+    return random.choice(names)
 
 
 def get_values(name:str=None)->[float]:
     """ Get a real world time series """
     mr = MicroReader()
     if name is None:
-        name = random.choice(DEFAULT_STREAMS)
+        name = random_name()
     lagged = mr.get_lagged_values(name=name)
     values = list(reversed(lagged))
     return values
