113ef6e86656f2e6030397606f86039cd9fafe4b
Peter Cotton <peter.cotton@microprediction.com>
2020-12-31 16:50:20 -0500

    whatever

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="realdata",
-    version="0.0.1",
+    version="0.0.2",
     description="Get real time series data easily for testing",
     long_description=README,
     long_description_content_type="text/markdown",
