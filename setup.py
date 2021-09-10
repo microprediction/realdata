338519138bbd1987263e592fe73b7b88623924cb
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-09-10 07:33:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="realdata",
-    version="0.0.6",
+    version="0.0.7",
     description="Get real univariate time series data easily for testing",
     long_description=README,
     long_description_content_type="text/markdown",
