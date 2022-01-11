820a3a26d20db4667019cc4f41828d6b9c24ff70
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-01-11 12:22:09 -0500

    Update test_stuff.py

--- tests/test_stuff.py
+++ tests/test_stuff.py
@@ -1,7 +1,9 @@
-from realdata import get_live, get_historical
+from realdata import get_live, get_historical, live, historical
 
 def test_live():
     values = get_live()
+    values = live()
     
 def test_historical():
     df = get_historical(n_obs=5000)
+    df = historical()
