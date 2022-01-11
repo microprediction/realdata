8212fc643da62b4b3242768dfa1dc21c94d412d8
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-01-11 11:51:56 -0500

    Update test_nothing.py

--- tests/test_nothing.py
+++ tests/test_nothing.py
@@ -1,2 +1,8 @@
-def test_nothing():
-    assert True
+from realdata import get_random_historical_data
+from realdata import get_values
+
+def test_live():
+    values = get_values()
+    
+def test_historical():
+    df = get_random_historical_data(n_obs=5000)
